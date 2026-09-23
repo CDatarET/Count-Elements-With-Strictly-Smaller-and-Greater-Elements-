fcb69e8f6c4d28a65cf2c7be2ce31b0f6ca49b11
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-09-23 09:43:12 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countElements(int[] nums) {
+        int min = nums[0];
+        int max = min;
+        for(int i = 1; i < nums.length; i++){
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        
+        int ret = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > min && nums[i] < max){
+                ret++;
+            }
+        }
+
+        return ret;
+    }
+}
